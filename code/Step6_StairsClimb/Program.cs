cd467874a3599a69ea255d7c6e985f4d5ccb981d
okolodk <grisha.zhivot@bk.ru>
2026-03-17 19:44:28 +0300

    Step 6: while and for loops with flowcharts

---
+++ code/Step6_StairsClimb/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Подъём по лестнице");
+Console.Write("На какой этаж нужно подняться?");
+int targetFloor = Convert.ToInt32(Console.ReadLine());
+int currentFloor = 1;
+Console.Write("Начинаем подъём");
+while(currentFloor<targetFloor)
+{
+    currentFloor++; 
+    Console.WriteLine($"Поднялись на {currentFloor} этаж");
+}
+Console.WriteLine($"Вы достигли {targetFloor} этажа");
+Console.WriteLine("Конец программы");
