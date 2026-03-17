3ea05747d8d66996bd20bdfffdebe9dc6a21a3dd
okolodk <grisha.zhivot@bk.ru>
2026-03-17 13:35:09 +0300

    Diagramm

--- code/Step3_WeatherChoice/Program.cs
+++ code/Step3_WeatherChoice/Program.cs
@@ -1,13 +1,14 @@
-﻿// Начало
-Console.WriteLine("Выбор одежды по погоде");
-Console.Write("Введите текущую температуру:");
-int temperatyre = Convert.ToInt32(Console.ReadLine());
-if (temperatyre >= 20)
-{
-    Console.WriteLine("Наденьте лёгкую одежду");
-}
-else
-{
-    Console.WriteLine("Наденьте тёплую одежду");
-}
-Console.WriteLine("Хорошего дня!");
+﻿// // Начало
+// Console.WriteLine("Выбор одежды по погоде");
+// Console.Write("Введите текущую температуру:");
+// int temperatyre = Convert.ToInt32(Console.ReadLine());
+// if (temperatyre >= 20)
+// {
+//     Console.WriteLine("Наденьте лёгкую одежду");
+// }
+// else
+// {
+//     Console.WriteLine("Наденьте тёплую одежду");
+// }
+// Console.WriteLine("Хорошего дня!");
+
