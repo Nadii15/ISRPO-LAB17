27bf7d4eaa6d15a162b9814d64933566c8632b2d
okolodk <grisha.zhivot@bk.ru>
2026-03-17 21:17:08 +0300

    Step 7 arrays

---
+++ code/Step7_Max_Run/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {5,12,8,23,445,17,9,31};
+Console.WriteLine("Поиск максимального элемента");
+Console.WriteLine("Массив" + string.Join(",",array));
+int max = array[0];
+for (int i=1;i<array.Length;i++)
+{
+  if (array[i] > max)
+    {
+        max = array[i];
+    }  
+}
+Console.WriteLine($"Максимальный элемент {max}");
---
+++ code/Step7_while_Run/Program.cs
@@ -0,0 +1,9 @@
+﻿int [] array = {5,12,8,23,45,17,9,31};
+Console.WriteLine("Вывод элементов масива");
+Console.WriteLine($"Длинна массива: {array.Length}");
+Console.WriteLine("Элемент массива");
+for (int i=0; i<array.Length;i++)
+{
+    Console.WriteLine($"{array[i]}");
+}
+Console.WriteLine("Конец программы");
